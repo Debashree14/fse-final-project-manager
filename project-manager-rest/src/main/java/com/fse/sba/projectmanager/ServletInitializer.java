ed01f3928d967a730babc90847fd590523a515e0
Debashree Dutta <debashree.dutta14@gmail.com>
2019-09-15 22:13:02 +0530

    Adding more functionality

--- project-manager-rest/src/main/java/com/fse/sba/projectmanager/ServletInitializer.java
+++ project-manager-rest/src/main/java/com/fse/sba/projectmanager/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.fse.sba.projectmanagerrest;
+package com.fse.sba.projectmanager;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ProjectManagerRestApplication.class);
+		return application.sources(ProjectManagerApplication.class);
 	}
 
 }
--- project-manager-ui/webpack.config.js
+++ project-manager-ui/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
     path: path.join(__dirname, "/dist"),
     filename: "index-bundle.js"
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
   module: {
     rules: [
       {
